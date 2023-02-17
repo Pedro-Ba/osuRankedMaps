1fd0b0f1a3549a3dc0811ed00a3dfe307f38c4c1
Estia <83793315+Pedro-Ba@users.noreply.github.com>
2023-02-16 22:15:46 -0300

    First complete product

--- osuAPIfunctions.js
+++ osuAPIfunctions.js
@@ -1,20 +1,21 @@
-var loginValues = require('./config.json');
-
-const url = new URL(
-    "https://osu.ppy.sh/oauth/token"
-);
-
-const headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/x-www-form-urlencoded",
-};
-
-let body = "client_id=" + loginValues.client_id +
-    "&client_secret=" + loginValues.client_secret +
-    "&grant_type=" + loginValues.grant_type +
-    "&scope=" + loginValues.scope;
+module.exports = {getAPIkey, lookupBeatmap, getBeatmapSetDisc}
 
 async function getAPIkey() {
+    var loginValues = require('./config.json');
+
+    const url = new URL(
+        "https://osu.ppy.sh/oauth/token"
+    );
+    
+    const headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+    };
+    
+    let body = "client_id=" + loginValues.client_id +
+        "&client_secret=" + loginValues.client_secret +
+        "&grant_type=" + loginValues.grant_type +
+        "&scope=" + loginValues.scope;
     return fetch(url, {
         method: "POST",
         headers,
@@ -22,4 +23,48 @@ async function getAPIkey() {
     }).then(response => response.json().then(json => json['access_token']));
 }
 
-getAPIkey();
+async function lookupBeatmap(api_key, id){
+    const url = new URL(
+        "https://osu.ppy.sh/api/v2/beatmaps/lookup"
+    );
+    
+    const params = {
+        "id": id,
+    };
+    Object.keys(params)
+        .forEach(key => url.searchParams.append(key, params[key]));
+    
+    const headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        Authorization: 'Bearer ' + api_key
+    };
+    
+    return fetch(url, {
+        method: "GET",
+        headers,
+    }).then(response => response.json());
+}
+
+async function getBeatmapSetDisc(api_key, beatmapsetid){
+    const url = new URL(
+        "https://osu.ppy.sh/api/v2/beatmapsets/discussions"
+    );
+    
+    const params = {
+        "beatmapset_id": beatmapsetid
+    };
+    Object.keys(params)
+        .forEach(key => url.searchParams.append(key, params[key]));
+    
+    const headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",        
+        Authorization: 'Bearer ' + api_key
+    };
+    
+    return fetch(url, {
+        method: "GET",
+        headers,
+    }).then(response => response.json().then(jsonresponse => jsonresponse['beatmaps']));
+}
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var osuAPI = require('./osuAPIfunctions.js');
+var api_key = '';
 
 async function returnFetchResponse(fetchLink) {
     return fetch(fetchLink, {
@@ -20,13 +21,77 @@ async function get2023beatmaps(fetchLink) {
 }
 
 async function writeBeatmapCSV(cleanInput) {
-    writeStream = fs.createWriteStream('beatmaps.csv', {'flags': 'a'});
+    writeStream = fs.createWriteStream('beatmaps.csv', { 'flags': 'a' });
     writeStream.write(cleanInput);
+    return;
 }
 
-async function iteratesThroughBeatmaps(beatmaps){
-    inputString = '';
-    for(const beatmap of beatmaps){
+async function writeDifficultiesCSV(cleanInput) {
+    writeStream = fs.createWriteStream('beatmapDiffs.csv', { 'flags': 'a' });
+    writeStream.write(cleanInput);
+    return;
+}
+
+async function iteratesThroughDifficulties(beatmapDiffs) {
+    let inputString = '';
+    let diffs = [];
+    for (i = 0; i < beatmapDiffs.length; i++) {
+        let beatmap = await osuAPI.lookupBeatmap(api_key, beatmapDiffs[i]);
+        let diffObject = {
+            'url': beatmap['url'],
+            'artist': beatmap['beatmapset']['artist'],
+            'title': beatmap['beatmapset']['title'],
+            'version': beatmap['version'],
+            'SR': beatmap['difficulty_rating'],
+            'AR': beatmap['ar'],
+            'OD': beatmap['accuracy'],
+            'CS': beatmap['cs'],
+            'HP': beatmap['drain'],
+            'Circle Count': beatmap['count_circles'],
+            'Slider Count': beatmap['count_sliders'],
+            'Spinner Count': beatmap['count_spinners']
+        }
+        diffs.push(diffObject);
+    }
+    diffs.sort(function (a, b) {
+        return (a.SR < b.SR ? -1 : -1);
+    })
+    for (i = 0; i < diffs.length; i++) {
+        inputString += (
+            diffs[i].url + ',' +
+            '"' + diffs[i].artist + '"' + ',' +
+            '"' + diffs[i].title + '"' + ',' +
+            '"' + diffs[i].version + '"' + ',' +
+            diffs[i].SR + ',' +
+            diffs[i].AR + ',' +
+            diffs[i].OD + ',' +
+            diffs[i].CS + ',' +
+            diffs[i].HP + ',' +
+            diffs[i]['Circle Count'] + ',' +
+            diffs[i]['Slider Count'] + ',' +
+            diffs[i]['Spinner Count'] + ',' +
+            '\n'
+        );
+    }
+    await writeDifficultiesCSV(inputString);
+    return;
+}
+
+async function iteratesThroughSet(beatmapSet) {
+    var len = beatmapSet.length;
+    let difficulties = [];
+    for (i = 0; i < len; i++) {
+        if (beatmapSet[i]['ranked'] == 1) {
+            difficulties.push(beatmapSet[i]['id']);
+        }
+    }
+    await iteratesThroughDifficulties(difficulties);
+    return;
+}
+
+async function iteratesThroughBeatmaps(beatmaps) {
+    let inputString = '';
+    for (const beatmap of beatmaps) {
         inputString += (
             beatmap['beatmapset'] + ',' +
             beatmap['beatmap_id'] + ',' +
@@ -38,13 +103,17 @@ async function iteratesThroughBeatmaps(beatmaps){
             beatmap['bpm'] + ',' +
             beatmap['total_length'] + ',' +
             beatmap['map_count'] + ','
-            +'\n'
+            + '\n'
         );
+        let beatmapSet = await osuAPI.getBeatmapSetDisc(api_key, beatmap['beatmapset_id']);
+        await iteratesThroughSet(beatmapSet);
     };
     await writeBeatmapCSV(inputString);
+    return;
 }
 
 async function main() {
+    api_key = await osuAPI.getAPIkey();
     let i = 0;
     const BaseURL = 'https://osusearch.com/query/?statuses=Ranked&modes=Standard&date_start=2023-01-01&offset='
     let fetchLink = BaseURL + i;
@@ -58,8 +127,4 @@ async function main() {
     }
 }
 
-// main();
-
-//done, csv written
-
-//next -> use beatmap id or beatmap setid to get further detailed description of each map diff
+main();
